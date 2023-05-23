2e479b435f0b26c94fe600243e4d936b8e6ebada
Nemo <88682289+amirhu37@users.noreply.github.com>
2023-05-24 01:26:22 +0330

    1st commit

---
+++ tel.py
@@ -0,0 +1,36 @@
+from telethon.sync import TelegramClient
+from telethon.tl.functions.channels import GetParticipantsRequest
+from telethon.tl.types import ChannelParticipantsSearch
+
+# Configure your API credentials
+api_id = 'api_code'
+api_hash = 'api_hash'
+
+# Configure your session file name
+session_name = 'session_file_name'
+
+# Configure the target channel username or ID
+channel_link  = -1001410159417
+
+# Initialize the Telegram client
+client = TelegramClient(session_name, api_id, api_hash)
+client.start()
+
+
+
+channel = client.get_entity(channel_link)
+messages = client.get_messages(channel, limit=1000)  # You can change the limit as needed
+
+
+# for message in messages:
+with open('channel_messages.txt', 'w', encoding='utf-8') as file:
+    for message in messages:
+        file.write(message.text + '\n')
+
+        print(message.text)
+
+
+client.disconnect()
+
+#     # Save messages to a file
+
